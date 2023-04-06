594caae4f9ef13526d682ede516145e33aa076fc
IgorArt <artushkin.igor@gmail.com>
2023-04-06 15:53:31 +0300

    install vue

---
+++ resources/js/App.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div>Laravel API + Vue SPA</div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+  plugins: [
+    vue(),
+    laravel({
+      input: ['resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+})
