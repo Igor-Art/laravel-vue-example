22ffe5e7dc4331ada3a654249a3421490ae585ba
IgorArt <artushkin.igor@gmail.com>
2023-04-08 10:00:12 +0300

    install pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
 import '../css/app.css'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+
+const app = createApp(App)
+
+app.use(pinia)
+
+app.mount('#app')
