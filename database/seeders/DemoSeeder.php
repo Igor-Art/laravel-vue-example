c62e14ddddba714d29e68cc3f05bfc830642ca90
IgorArt <artushkin.igor@gmail.com>
2023-04-10 20:16:59 +0300

    demo seeder

---
+++ database/seeders/DemoSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class DemoSeeder extends Seeder
+{
+    public function run(): void
+    {
+        if (User::query()->exists()) {
+            return;
+        }
+
+        File::copyDirectory(resource_path('stubs/images'), Storage::disk('public')->path('/'));
+
+        $this->call(UserSeeder::class);
+        $this->call(GameSeeder::class);
+    }
+}
