1f585c8284af7d1eecd0a89233211409aaa93a08
IgorArt <artushkin.igor@gmail.com>
2023-04-15 11:03:21 +0300

    navigation fade transition

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -8,6 +8,21 @@ import HeaderNav from '@/components/Layout/HeaderNav.vue'
     <HeaderNav />
   </header>
   <main class="container pt-8 pb-2">
-    <RouterView />
+    <RouterView v-slot="{ Component, route }">
+      <Transition name="fade" mode="out-in" appear>
+        <Component :is="Component" :key="route.path" />
+      </Transition>
+    </RouterView>
   </main>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .4s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -5,30 +5,32 @@ import Headline from '@/components/Headline.vue'
 </script>
 
 <template>
-  <div class="mb-10">
-    <Headline icon="code">Disclaimer</Headline>
-    <div class="pl-2 leading-6 font-light">
-      <p>
-        Welcome to the Laravel-Vue example application.
-        This is not a realistic, but close to reality SPA-API app.
-      </p>
-      <p>
-        Designed for junior developers, show the main points of developing
-        a classic web app with Auth, Roles, CRUD, Service Layer, etc.
-      </p>
-      <p>
-        Your application may have a different implementation.
-        Use any code of the app at your own risk 😈.
-      </p>
-      <p>
-        You can make a suggestion or fix something in a github issue. Enjoy!
-      </p>
+  <div>
+    <div class="mb-10">
+      <Headline icon="code">Disclaimer</Headline>
+      <div class="pl-2 leading-6 font-light">
+        <p>
+          Welcome to the Laravel-Vue example application.
+          This is not a realistic, but close to reality SPA-API app.
+        </p>
+        <p>
+          Designed for junior developers, show the main points of developing
+          a classic web app with Auth, Roles, CRUD, Service Layer, etc.
+        </p>
+        <p>
+          Your application may have a different implementation.
+          Use any code of the app at your own risk 😈.
+        </p>
+        <p>
+          You can make a suggestion or fix something in a github issue. Enjoy!
+        </p>
+      </div>
     </div>
-  </div>
-  <div class="mb-6">
-    <GamesTopSection />
-  </div>
-  <div class="mb-10">
-    <ReviewsLastSection />
+    <div class="mb-6">
+      <GamesTopSection />
+    </div>
+    <div class="mb-10">
+      <ReviewsLastSection />
+    </div>
   </div>
 </template>
