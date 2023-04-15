66cd6876d2170fafce8b0bac94d1a56190270b3d
IgorArt <artushkin.igor@gmail.com>
2023-04-15 11:43:44 +0300

    not found page ascii art

--- resources/js/views/404.vue
+++ resources/js/views/404.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+<script setup>
+import Headline from '@/components/Headline.vue'
+</script>
 
 <template>
-  <div>
-    404 Not Found
+  <div class="flex items-center">
+    <Headline class="mr-8">404 Not Found</Headline>
+      <pre class="whitespace-pre-wrap">
+  ／l、
+（ﾟ､ ｡７
+  l、ﾞ~ヽ
+  じしf_,)ノ</pre>
   </div>
 </template>
