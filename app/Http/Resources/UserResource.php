e0e30e7bafd1047e23005c3333d1a0beae55f056
Igor <artushkin.igor@gmail.com>
2023-08-22 22:14:14 +0300

    Fix auth (#3)
    * fix auth responses
    
    * fix 2fa form input

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserResource extends JsonResource
 {
+    public static $wrap = null;
+
     public function toArray(Request $request): array
     {
         return [
